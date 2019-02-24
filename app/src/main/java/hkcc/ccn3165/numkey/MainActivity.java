7fe2189a2dcacca6497dc1ef0105243b96ae03ae
johnnkp <johnnkp@hotmail.com>
2019-02-24 16:32:06 +0800

    Initial commit

---
+++ app/src/main/java/hkcc/ccn3165/numkey/MainActivity.java
@@ -0,0 +1,13 @@
+package hkcc.ccn3165.numkey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
